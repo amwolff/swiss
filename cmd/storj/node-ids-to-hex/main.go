6efecd8df9e9b82bc32123372402c09f7962cac1
Artur M. Wolff <artur.m.wolff@gmail.com>
2024-10-28 13:15:28 +0100

    cmd/storj: add node-ids-to-hex

---
+++ cmd/storj/node-ids-to-hex/main.go
@@ -0,0 +1,54 @@
+// usage: node-ids-to-query path/to/nodes.csv
+//
+// nodes.csv is
+//
+// id1,…
+// id2,…
+// …
+
+package main
+
+import (
+	"encoding/csv"
+	"encoding/hex"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"storj.io/common/storj"
+)
+
+func main() {
+	flag.Parse()
+
+	if n := flag.NArg(); n != 1 {
+		log.Fatalf("not enough/too many args (has %d, needs 1)", n)
+	}
+
+	f, err := os.Open(flag.Arg(0))
+	if err != nil {
+		log.Panicf("Open: %v", err)
+	}
+	defer f.Close()
+
+	r := csv.NewReader(f)
+
+	for i := 0; ; i++ {
+		records, err := r.Read()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			log.Panicf("Read: %v", err)
+		}
+		id, err := storj.NodeIDFromString(records[0])
+		if err != nil {
+			log.Printf("couldn't decode %q: %v", records[0], err)
+			continue
+		}
+		fmt.Println(hex.EncodeToString(id.Bytes()))
+	}
+}
